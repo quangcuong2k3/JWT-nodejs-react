39f8e3bcec165706fd8ea70d13f86a583a476a46
Cuong <cuongquangle539@gmail.com>
2025-04-06 09:58:31 +0700

    7

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,13 +1,10 @@
 import bcrypt from 'bcryptjs';
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
+import bluebird from 'bluebird';
+
 
 const salt = bcrypt.genSaltSync(10);
 //create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'jwt'
-});
 
 const hashUserPassword = (userPassword) => {
     let hashPassword = bcrypt.hashSync(userPassword, salt);
@@ -19,24 +16,37 @@ const createNewUser = (email, password, username) => {
 
     connection.query(
         'INSERT INTO users (email, password, username) VALUES (?, ?, ?)', [email, hashPass, username],
-        function (err, result, fields) {
+        function (err, results, fields) {
             if (err) {
                 console.log(err)
             }
         }
     );
 }
-const getUserList = () => {
-    let user = [];
-    connection.query(
-        'SELECT * FROM users',
-        function (err, result, fields) {
-            if (err) {
-                console.log(err)
-            }
-            console.log("check result:", result)
-        }
-    );
+const getUserList = async () => {
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird })
+
+    let users = [];
+    // connection.query(
+    //     'SELECT * FROM users',
+    //     function (err, results, fields) {
+    //         if (err) {
+    //             console.log(err);
+    //             return users;
+    //         }
+    //         users = results;
+    //         console.log("check list user:", users)
+    //         return users;
+    //     }
+    // );
+    try {
+        const [rows, fields] = await connection.execute('SELECT * FROM users');
+        return rows;
+    } catch (e) {
+        console.log("check eroor:", e)
+    }
+
+
 }
 module.exports = {
     createNewUser, getUserList
