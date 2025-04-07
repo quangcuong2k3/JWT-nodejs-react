1698e37d00390eb4e90d5be233fc5c72322d2cf6
Cuong <cuongquangle539@gmail.com>
2025-04-07 09:56:44 +0700

    5.4

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,10 +4,12 @@ import homeController from "../controller/homeController"
 const router = express.Router();
 
 const initWebRoutes = (app) => {
-    router.get("/", homeController.handleHelloWorld)
-    router.get("/user", homeController.handleUserPage)
-    router.post("/users/create-user", homeController.handleCreateNewUser)
-    router.post("/delete-user/:id", homeController.handleDeleteUser)
+    router.get("/", homeController.handleHelloWorld);
+    router.get("/user", homeController.handleUserPage);
+    router.post("/users/create-user", homeController.handleCreateNewUser);
+    router.post("/delete-user/:id", homeController.handleDeleteUser);
+    router.get("/update-user/:id", homeController.getUpdateUserPage);
+    router.post("/users/update-user", homeController.handleUpdateUser);
     return app.use("/", router)
 }
 export default initWebRoutes;
