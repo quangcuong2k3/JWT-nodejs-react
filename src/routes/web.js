c8634e4832a7da6cd4909ea62b371f28d9e5fcb3
Cuong <cuongquangle539@gmail.com>
2025-04-22 08:23:44 +0700

    9.4.2 test api

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,15 +1,25 @@
 import express from "express";
-import homeController from "../controller/homeController"
+import homeController from "../controller/homeController";
+import apiController from "../controller/apiController";
+
 
 const router = express.Router();
 
 const initWebRoutes = (app) => {
+    //path, handler
     router.get("/", homeController.handleHelloWorld);
     router.get("/user", homeController.handleUserPage);
     router.post("/users/create-user", homeController.handleCreateNewUser);
     router.post("/delete-user/:id", homeController.handleDeleteUser);
     router.get("/update-user/:id", homeController.getUpdateUserPage);
     router.post("/users/update-user", homeController.handleUpdateUser);
+
+    //rest api
+    //GET ,POST,DELETE,PUT
+    router.get("/api/test-api", apiController.testApi);
+
     return app.use("/", router)
+
+
 }
 export default initWebRoutes;
