2c893063a37c2d11e564e444ce96d1b3c6f8cf01
Cuong <cuongquangle539@gmail.com>
2025-04-24 22:11:45 +0700

    10.2.1 viet them API cho users

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -1,3 +1,5 @@
+import loginRegisterService from '../service/loginRegisterService';
+
 
 const testApi = (req, res) => {
     return res.status(200).json({
@@ -5,7 +7,38 @@ const testApi = (req, res) => {
         data: "test api"
     })
 }
-const handleRegister = (req, res) => {
+const handleRegister = async (req, res) => {
+    try {
+        if (!req.body.email || !req.body.phone || !req.body.password) {
+            return res.status(200).json({
+                EM: 'Missing required paramenter!',
+                EC: '1',
+                DT: ''
+            })
+        }
+        if (req.body.password && req.body.password.length < 4) {
+            return res.status(200).json({
+                EM: 'Your password must have more than 3 letter',
+                EC: '1',
+                DT: ''
+            })
+        }
+        //service create user
+        let data = await loginRegisterService.registerNewUser(req.body)
+
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: ''
+        })
+    } catch (e) {
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '-1',
+            DT: ''
+
+        })
+    }
     console.log("call me", req.body)
 }
 
